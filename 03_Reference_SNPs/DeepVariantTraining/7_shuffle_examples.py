52acfce9be1bf04e17fee713a4e60c9347dec751
Joseph Guhlin <joseph.guhlin@gmail.com>
2019-12-04 19:46:17 +0000

    Different shuffle script

--- 03_Reference_SNPs/DeepVariantTraining/7_shuffle_examples.py
+++ 03_Reference_SNPs/DeepVariantTraining/7_shuffle_examples.py
@@ -34,6 +34,8 @@ tf.compat.v1.enable_eager_execution(
 shuffle_tmp_files = 2048
 out_fhs = list()
 
+examples = 0
+
 for i in range(shuffle_tmp_files):
 	filename = "./shuffle_tmp/" + str(i) + ".examples";
 	fh = tf.io.TFRecordWriter(filename)
@@ -47,7 +49,10 @@ for raw_record in itershuffle(raw_dataset, 1000000):
   example.ParseFromString(raw_record.numpy())
   writer = random.choice(out_fhs)
   writer.write(example.SerializeToString())
+  examples = examples + 1
   # print(raw_record)
+
+print("Total examples: " + str(examples));
   
 shuffle_tmp_files = 2048
 out_fhs = list()
@@ -88,8 +93,13 @@ for raw_record in itershuffle(raw_dataset, 1500000):
 inputs = glob.glob("./shuffle_tmp3/*examples");
 
 out_fhs = list()
+shuffle_tmp_files = 2048
+out_fhs = list()
 
-writer = tf.io.TFRecordWriter("training_examples_shuffled/shuffled.examples")
+for i in range(shuffle_tmp_files):
+        filename = "./training_examples_shuffled/" + str(i) + ".examples";
+        fh = tf.io.TFRecordWriter(filename)
+        out_fhs.append(fh)
 
 raw_dataset = tf.data.TFRecordDataset(inputs)
 for raw_record in itershuffle(raw_dataset, 10000000):
